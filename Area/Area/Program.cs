f31fe39955ce5f2b0697a512e6a096aded90b521
EdwardCadena <earodriguez154@misena.edu.co>
2020-11-07 13:23:15 -0500

    new test

---
+++ Area/Area/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xa;
+            int x, y;
+            int cal;
+            Console.WriteLine("Digite el numero de la figura que desea obtener el area");
+            Console.WriteLine("1 - circulo, 2 - rectangulo");
+            cal = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el valor de x");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el valor de y");
+            y = int.Parse(Console.ReadLine());
+
+            Shape objshape = new Shape(x, y);
+            xa = objshape.area();
+            if (cal == 2)
+            {
+                Console.WriteLine("El area del rectangulo" + xa);
+                Console.ReadLine();
+            }
+            Console.WriteLine("El area del circulo" + xa);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Area/Area/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area
+{
+    class Shape
+    {
+        private int ya;
+        private int xa;
+        public Shape(int x, int y)
+        {
+            xa = x;
+            ya = y ;
+        }
+
+        public double area()
+        {
+            double result;
+            result = xa * ya;
+            return result;
+        }
+    }
+}
